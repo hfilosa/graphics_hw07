6e492ead9d4cfa67878ee10760edcb8f14bc7998
Henry Filosa <henryfilosa@henrys-imac.home>
2016-04-23 21:11:26 -0400

    Got everything except for save to work. Some where issues with the string being saved in the union. May be an issue with DW base code

--- my_main.c
+++ my_main.c
@@ -60,13 +60,81 @@ void my_main( int polygons ) {
   struct stack *s;
   screen t;
   color g;
+
+  g.red = 0;
+  g.green = 255;
+  g.blue = 0;
   
   s = new_stack();
   tmp = new_matrix(4, 1000);
   clear_screen( t );
 
   for (i=0;i<lastop;i++) {  
-    switch (op[i].opcode) {
+    switch (op[i].opcode){
+    case LINE:
+      add_edge(tmp,op[i].op.line.p0[0],op[i].op.line.p0[1],op[i].op.line.p0[2], op[i].op.line.p1[0], op[i].op.line.p1[1], op[i].op.line.p1[2]);
+      matrix_mult(s->data[ s->top ],tmp);
+      draw_lines(tmp,t,g);
+      tmp->lastcol = 0;
+      break;
+    case SPHERE:
+      add_sphere(tmp,op[i].op.sphere.d[0],op[i].op.sphere.d[1],op[i].op.sphere.d[2],op[i].op.sphere.r,10);
+      matrix_mult(s->data[ s->top ],tmp);
+      draw_polygons(tmp,t,g);
+      tmp->lastcol = 0;
+      break;
+    case TORUS:
+      add_torus(tmp,op[i].op.torus.d[0],op[i].op.torus.d[1],op[i].op.torus.d[2],op[i].op.torus.r0,op[i].op.torus.r1,10);
+      matrix_mult(s->data[ s->top ],tmp);
+      draw_polygons(tmp,t,g);
+      tmp->lastcol = 0;
+      break;
+    case BOX:
+      add_box(tmp,op[i].op.box.d0[0],op[i].op.box.d0[1],op[i].op.box.d0[2],op[i].op.box.d1[0],op[i].op.box.d1[1],op[i].op.box.d1[2]);
+      matrix_mult(s->data[ s->top ],tmp);
+      draw_polygons(tmp,t,g);
+      tmp->lastcol = 0;
+      break;
+    case SCALE:
+      transform=make_scale(op[i].op.scale.d[0],op[i].op.scale.d[1],op[i].op.scale.d[2]);
+      matrix_mult(s->data[ s->top ],transform);
+      copy_matrix(transform,s->data[ s->top ]);
+      free_matrix(transform);
+      break;
+    case ROTATE:
+      if (op[i].op.rotate.axis == 0)
+	transform=make_rotX(op[i].op.rotate.degrees*(M_PI / 180));
+      if (op[i].op.rotate.axis == 1)
+	transform=make_rotY(op[i].op.rotate.degrees*(M_PI / 180));
+      if (op[i].op.rotate.axis == 2)
+	transform=make_rotZ(op[i].op.rotate.degrees*(M_PI / 180));
+      matrix_mult(s->data[ s->top ],transform);
+      copy_matrix(transform,s->data[ s->top ]);
+      free_matrix(transform);
+      break;
+    case MOVE:
+      transform=make_translate(op[i].op.move.d[0],op[i].op.move.d[1],op[i].op.move.d[2]);
+      matrix_mult(s->data[ s->top ],transform);
+      copy_matrix(transform,s->data[ s->top ]);
+      free_matrix(transform);
+      break;
+    case POP:
+      pop(s);
+      break;
+    case PUSH:
+      push(s);
+      break;
+    case DISPLAY:
+      display(t);
+      break;
+    case SAVE:
+      //Save doesn't work for some reason :(
+      printf("%s\n",op[i].op.save);
+      printf("%s\n",op[i].op.save.p);
+      //save_extension(t,op[i].op.save.p);
+      break;
     }
   }
+  free_matrix(tmp);
+  free_stack(s);
 }
